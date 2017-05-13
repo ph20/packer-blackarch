41bff2c67e0eb46ec9db687079c5e008327567e0
Alexander Grynchuk <agrynchuk@cloudlinux.com>
2017-05-13 14:02:18 +0300

    Added free space check before build

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+FREE=`df -k --output=avail "$PWD" | tail -n1`   # df -k not df -h
+if [[ $FREE -lt 26214400 ]]; then               # 25G = 25*1024*1024k
+     echo "for building need ~25G free space"; exit 1;
+fi;
 VAR_FILE=$(python2 genvars.py)
 packer-io build -var-file=$VAR_FILE -only=virtualbox-iso blackarch-template.json
 packer-io build -var-file=$VAR_FILE -only=qemu blackarch-template.json
