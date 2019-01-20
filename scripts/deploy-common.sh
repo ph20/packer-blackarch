4234c8353faf507d923be92edb7046215fa9d055
Alexandr Grynchuk <agrynchuk@gmail.com>
2019-01-20 10:36:22 +0200

    Fixed installing yaourt package

--- scripts/deploy-common.sh
+++ scripts/deploy-common.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 /usr/bin/sed -i 's/#X11Forwarding no/X11Forwarding yes/' /etc/ssh/sshd_config # enable X11 forwarding for run gui apps
 
-/usr/bin/pacman -S --noconfirm --overwrite --needed --quiet --noprogressbar --cachedir=/vagrant/pkg_cache yaourt mlocate net-tools strace vim rsync
+/usr/bin/pacman -S --noconfirm --overwrite --needed --quiet --noprogressbar --cachedir=/vagrant/pkg_cache mlocate net-tools strace vim rsync
 # Fix package-query: error while loading shared libraries: libalpm.so.10: cannot open shared object file: No such file or directory
 ln -s /usr/lib/libalpm.so.11 /usr/lib/libalpm.so.10
 /usr/bin/pacman -S --noconfirm --overwrite --needed --quiet --noprogressbar --cachedir=/vagrant/pkg_cache \
